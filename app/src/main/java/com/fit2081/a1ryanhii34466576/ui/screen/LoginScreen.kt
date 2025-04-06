e44e686a757fedb2369951b5820358fd30e0bab9
Ryan Zhi Yin Hii <rhii0001@student.monash.edu>
2025-04-06 13:01:35 +1000

    Add navigation and validation to LoginScreen

--- app/src/main/java/com/fit2081/a1ryanhii34466576/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/fit2081/a1ryanhii34466576/ui/screen/LoginScreen.kt
@@ -1,7 +1,6 @@
 package com.fit2081.a1ryanhii34466576.ui.screen
 
 import android.content.Context
-import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.material3.ExposedDropdownMenuDefaults.TrailingIcon
@@ -9,20 +8,22 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.fit2081.a1ryanhii34466576.data.CsvReader
-import com.fit2081.a1ryanhii34466576.data.User
+import com.fit2081.a1ryanhii34466576.CsvReader
+import com.fit2081.a1ryanhii34466576.User
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(navController: NavController) {
+    val context = LocalContext.current
+    val userData: List<User> = CsvReader.loadUserData(context)
+
     var selectedUserId by remember { mutableStateOf("") }
     var phoneNumber by remember { mutableStateOf("") }
     var expanded by remember { mutableStateOf(false) }
-    val context: Context = LocalContext.current
-    val userData: List<User> =
-        CsvReader.loadUserData(context) // Load user data from CSV
+    var isUserValid by remember { mutableStateOf(true) }
 
     Column(
         modifier = Modifier
@@ -31,9 +32,13 @@ fun LoginScreen(navController: NavController) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        Text(text = "Login Screen", style = MaterialTheme.typography.titleLarge)
-        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Log in",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold
+        )
 
+        Spacer(modifier = Modifier.height(16.dp))
 
         // Referenced from: https://medium.com/@german220291/building-a-custom-exposed-dropdown-menu-with-jetpack-compose-d65232535bf2
         ExposedDropdownMenuBox(
@@ -45,7 +50,7 @@ fun LoginScreen(navController: NavController) {
                 readOnly = true,
                 value = selectedUserId,
                 onValueChange = {},
-                label = { Text(text = "ID") },
+                label = { Text(text = "Select User ID") },
                 trailingIcon = {
                     TrailingIcon(expanded = expanded)
                 },
@@ -70,7 +75,6 @@ fun LoginScreen(navController: NavController) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Text field for Phone Number
         OutlinedTextField(
             value = phoneNumber,
             onValueChange = { phoneNumber = it },
@@ -78,20 +82,45 @@ fun LoginScreen(navController: NavController) {
             modifier = Modifier.fillMaxWidth(),
         )
 
+        if (!isUserValid) {
+            Text(
+                text = "Incorrect phone number",
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(top = 2.dp)
+            )
+        }
+
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Continue Button
         Button(
             onClick = {
                 val user = userData.find { it.userId == selectedUserId }
 
                 if (user != null && user.phoneNumber == phoneNumber) {
-                    Toast.makeText(context, "Login Successful", Toast.LENGTH_LONG).show()
-                    navController.navigate("questionnaire")
+                    isUserValid = true
+                    val userPref =
+                        context.getSharedPreferences(selectedUserId, Context.MODE_PRIVATE)
+
+                    if (userPref.contains("selectedCategories")) {
+                        navController.navigate("home/$selectedUserId") {
+                            popUpTo("welcome") { inclusive = true }
+                        }
+                    } else {
+                        navController.navigate("questionnaire/$selectedUserId") {
+                            popUpTo("welcome") { inclusive = true }
+                        }
+                    }
+
                 } else {
-                    Toast.makeText(context, "Incorrect Credentials", Toast.LENGTH_LONG).show()
+                    isUserValid = false
                 }
-            }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
         ) {
             Text(text = "Continue")
         }
