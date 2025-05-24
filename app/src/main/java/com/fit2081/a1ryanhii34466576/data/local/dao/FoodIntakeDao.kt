ba8b1885440cc8801c0dcad45cb667db70d2c5b3
Ryan Zhi Yin Hii <rhii0001@student.monash.edu>
2025-05-25 07:50:29 +1000

    Update FoodIntakeDao to include new query methods for retrieving the latest and all food intakes, and remove onConflict strategy from insert method.

--- app/src/main/java/com/fit2081/a1ryanhii34466576/data/local/dao/FoodIntakeDao.kt
+++ app/src/main/java/com/fit2081/a1ryanhii34466576/data/local/dao/FoodIntakeDao.kt
@@ -2,15 +2,18 @@ package com.fit2081.a1ryanhii34466576.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.fit2081.a1ryanhii34466576.data.model.FoodIntake
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FoodIntakeDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert
     suspend fun insertFoodIntake(foodIntake: FoodIntake)
 
-    @Query("SELECT * FROM food_intakes WHERE userId = :userId")
-    suspend fun getFoodIntakeById(userId: String): FoodIntake?
+    @Query("SELECT * FROM food_intakes WHERE userId = :userId ORDER BY timestamp DESC LIMIT 1")
+    suspend fun getLatestFoodIntake(userId: String): FoodIntake?
+
+    @Query("SELECT * FROM food_intakes WHERE userId = :userId ORDER BY timestamp DESC")
+    fun getAllFoodIntakes(userId: String): Flow<List<FoodIntake>>
 }
