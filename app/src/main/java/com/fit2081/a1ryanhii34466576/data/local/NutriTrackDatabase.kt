8786f72528dcb7f653339a5f8612cd350c5498b2
Ryan Zhi Yin Hii <rhii0001@student.monash.edu>
2025-05-25 03:42:03 +1000

    Add NutriTrackDatabase class to manage Room database for Patient, FoodIntake, and NutriCoachTip entities

---
+++ app/src/main/java/com/fit2081/a1ryanhii34466576/data/local/NutriTrackDatabase.kt
@@ -0,0 +1,40 @@
+package com.fit2081.a1ryanhii34466576.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.fit2081.a1ryanhii34466576.data.local.dao.FoodIntakeDao
+import com.fit2081.a1ryanhii34466576.data.local.dao.NutriCoachTipDao
+import com.fit2081.a1ryanhii34466576.data.local.dao.PatientDao
+import com.fit2081.a1ryanhii34466576.data.model.FoodIntake
+import com.fit2081.a1ryanhii34466576.data.model.NutriCoachTip
+import com.fit2081.a1ryanhii34466576.data.model.Patient
+
+@Database(
+    entities = [Patient::class, FoodIntake::class, NutriCoachTip::class],
+    version = 4,
+    exportSchema = false
+)
+abstract class NutriTrackDatabase : RoomDatabase() {
+    abstract fun patientDao(): PatientDao
+    abstract fun foodIntakeDao(): FoodIntakeDao
+    abstract fun nutriCoachTipDao(): NutriCoachTipDao
+
+    companion object {
+        @Volatile
+        private var Instance: NutriTrackDatabase? = null
+
+        fun getDatabase(context: Context): NutriTrackDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    NutriTrackDatabase::class.java,
+                    "nutritrack_database"
+                )
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
