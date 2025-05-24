220eaee37d84e261dc5bb4f517cd926094950c5f
Ryan Zhi Yin Hii <rhii0001@student.monash.edu>
2025-05-25 07:50:42 +1000

    Update FoodIntakeRepository to rename food intake retrieval method and add flow for all food intakes.

--- app/src/main/java/com/fit2081/a1ryanhii34466576/data/repository/FoodIntakeRepository.kt
+++ app/src/main/java/com/fit2081/a1ryanhii34466576/data/repository/FoodIntakeRepository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.fit2081.a1ryanhii34466576.data.local.NutriTrackDatabase
 import com.fit2081.a1ryanhii34466576.data.model.FoodIntake
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 
 class FoodIntakeRepository(context: Context) {
@@ -12,6 +13,9 @@ class FoodIntakeRepository(context: Context) {
     suspend fun insertFoodIntake(foodIntake: FoodIntake) =
         withContext(Dispatchers.IO) { foodIntakeDao.insertFoodIntake(foodIntake) }
 
-    suspend fun getFoodIntake(userId: String): FoodIntake? =
-        withContext(Dispatchers.IO) { foodIntakeDao.getFoodIntakeById(userId) }
+    suspend fun getLatestFoodIntake(userId: String): FoodIntake? =
+        withContext(Dispatchers.IO) { foodIntakeDao.getLatestFoodIntake(userId) }
+
+    fun getAllFoodIntakes(userId: String): Flow<List<FoodIntake>> =
+        foodIntakeDao.getAllFoodIntakes(userId)
 }
