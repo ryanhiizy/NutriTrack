ecec5f5f31505a492deee565d1614b49c82b6366
Ryan Zhi Yin Hii <rhii0001@student.monash.edu>
2025-05-25 04:20:50 +1000

    Add FruityViceApi interface for fetching fruit data by name using Retrofit

---
+++ app/src/main/java/com/fit2081/a1ryanhii34466576/data/api/FruityViceApi.kt
@@ -0,0 +1,10 @@
+package com.fit2081.a1ryanhii34466576.data.api
+
+import com.fit2081.a1ryanhii34466576.ui.screen.nutricoach.Fruit
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface FruityViceApi {
+    @GET("api/fruit/{name}")
+    suspend fun getFruitByName(@Path("name") name: String): Fruit
+}
