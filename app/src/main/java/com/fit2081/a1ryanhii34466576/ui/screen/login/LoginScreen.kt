232e4a3b33089fd51df81ca78ea0e328a095abbc
Ryan Zhi Yin Hii <rhii0001@student.monash.edu>
2025-05-25 17:13:59 +1000

    Enhance LoginScreen with vertical scrolling capability to improve user experience on smaller devices

--- app/src/main/java/com/fit2081/a1ryanhii34466576/ui/screen/login/LoginScreen.kt
+++ app/src/main/java/com/fit2081/a1ryanhii34466576/ui/screen/login/LoginScreen.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
@@ -61,7 +63,8 @@ fun LoginScreen(navController: NavController, loginViewModel: LoginViewModel = v
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Log In", style = MaterialTheme.typography.titleLarge, fontWeight = FontWeight.Bold)
